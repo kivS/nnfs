fdbb42de982c8e0d3484a82fe0a38eba371c8a81
Vik Borges <v1k@protonmail.com>
2021-01-15 16:42:02 +0000

    pew pew

---
+++ nn_1.py
@@ -0,0 +1,14 @@
+'''
+	Single neuron with Numpy dot product
+'''
+import numpy as np 
+
+inputs = [1.0, 2.0, 3.0, 2.5]
+
+weights = [0.2, 0.8, -0.5, 1.0]
+
+bias = 2.0
+
+
+output = np.dot(inputs, weights) + bias
+print(output)
